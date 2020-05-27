7b13c20faf1472261daea771c19e8103f8e59dd9
Arturo <arturo.anguix.l@gmail.com>
2020-05-27 20:17:05 +0200

    Update python script to use Adafruit library

--- read-sensor.py
+++ read-sensor.py
@@ -1,1 +1,9 @@
-print('26.5|70')
+import Adafruit_DHT
+DHT_SENSOR = Adafruit_DHT.DHT22
+DHT_PIN = 4
+
+humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+if humidity is not None and temperature is not None:
+    print(temperature + '|' + humidity)
+else:
+    print("Error|Error")
