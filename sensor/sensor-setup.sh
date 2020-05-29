2b76f71ca54e4fb35aa8cac4611eb448d4091ba5
Arturo <arturo.anguix.l@gmail.com>
2020-05-29 19:38:22 +0200

    Use new node version on the rapsberries

--- sensor/sensor-setup.sh
+++ sensor/sensor-setup.sh
@@ -4,5 +4,5 @@ sudo apt-get dist-upgrade
 sudo apt-get install python3-pip && sudo python3 -m pip install --upgrade pip setuptools wheel
 sudo pip3 install Adafruit_DHT
 
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
