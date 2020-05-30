a2ed429b58eaea76aa25c30314bfde87e12d090a
Arturo <arturo.anguix.l@gmail.com>
2020-05-30 11:01:21 +0200

    Create test file

--- sensor/sensor-setup.sh
+++ sensor/sensor-setup.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 yes | sudo apt-get update
 yes | sudo apt-get upgrade
-yes | sudo apt upgrade python3
 yes | sudo apt-get install python3-pip && yes | sudo pip3 install --upgrade setuptools
 
 yes | pip3 install RPI.GPIO
-yes |pip3 install adafruit-blinka
+yes | pip3 install adafruit-blinka
 yes | pip3 install adafruit-circuitpython-dht
 yes | sudo apt-get install libgpiod2
 
