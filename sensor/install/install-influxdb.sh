06e04a34e18d10084eaee9ea6de21d8b52e83748
Arturo <arturo.anguix.l@gmail.com>
2020-06-05 18:09:36 +0200

    Script to install influxdb

---
+++ sensor/install/install-influxdb.sh
@@ -0,0 +1,6 @@
+curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+echo "deb https://repos.influxdata.com/debian stretch stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+sudo apt update
+sudo apt install influxdb  
+sudo systemctl enable influxdb
+sudo systemctl start influxdb
