2e0185ee0380a6d685d61d8c4c70247e18fc324f
Arturo <arturo.anguix.l@gmail.com>
2020-06-21 19:10:53 +0200

    Catch error

--- server/test.js
+++ server/test.js
@@ -2,9 +2,10 @@ const { write, databaseNames } = require('./influxDb');
 const moment = require("moment")
 
 const start = async function(a, b) {
-    const localTime = moment().format()
-    console.log(await databaseNames())
     try {
+        const localTime = moment().format()
+        databases = await databaseNames()
+        console.log(databases)
         write('NAVE1', 25.3, 70.2, localTime)
     } catch (e) {
         console.error(e);
