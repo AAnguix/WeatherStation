85b1bba507c784f1553fd7f6829919227c15c59a
Arturo <arturo.anguix.l@gmail.com>
2020-06-21 19:26:32 +0200

    Test db writes

--- server/test.js
+++ server/test.js
@@ -4,11 +4,7 @@ const moment = require("moment")
 const start = async function(a, b) {
     try {
         const localTime = moment().format()
-        databaseNames().then(names => {
-            console.log(names)
-        })
-        .catch(error => console.log({ error }));
-        //write('NAVE1', 25.3, 70.2, localTime)
+        write('NAVE1', 25.3, 70.2, localTime)
     } catch (e) {
         console.error(e);
     }
