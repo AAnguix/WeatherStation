2c672693b50da1d8a7acd3d6b2b02e229e718449
Arturo <arturo.anguix.l@gmail.com>
2020-06-25 20:09:53 +0200

    Add dirname

--- server/config.js
+++ server/config.js
@@ -1,11 +1,10 @@
 const dotenv = require('dotenv');
-const result = dotenv.config();
+const result = dotenv.config({ path: __dirname + '/.env' })
 if (result.error) {
     throw result.error;
   }
   
 module.exports = {
-  port: process.env.PORT,
   sensor_ips: process.env.SENSORS.split(","),
   sensor_api_port: process.env.SENSOR_API_PORT,
   sensor_api_url: process.env.SENSOR_API_URL,
